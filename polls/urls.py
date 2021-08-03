74f6386e2ab1c8d1e33d37e0b1b4f4851475c037
mangoseeker <justctls1@gmail.com>
2021-08-03 12:57:11 +0300

    navigation between pages

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
-
+# home/ creating new path for home.html, path is home/home now
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.home, name="home"),
+    path('index/', views.index, name='index'),
+    path('books/', views.books, name='books'),
+    path('home/', views.home, name="home"),
+
 ]
